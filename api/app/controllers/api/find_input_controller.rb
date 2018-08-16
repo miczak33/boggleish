dd27b1053d8a946a331ea42a2feb4878bbf898e5
Micah Zakahi <mzakahi@lessonomix.com>
2018-08-16 01:49:01 -0700

    refactor api call a bit

--- api/app/controllers/api/find_input_controller.rb
+++ api/app/controllers/api/find_input_controller.rb
@@ -10,52 +10,10 @@ require 'openssl'
       found = @board.find_in_board(params[:input_text])
       isWord = false
       if found
-        url = URI.parse("https://od-api.oxforddictionaries.com/api/v1/entries/en/#{params[:input_text]}/regions=us")
-        puts url.host
-        puts url.port
-        https = Net::HTTP.new(url.host, url.port)
-        https.use_ssl = true
-        req = Net::HTTP::Get.new(url.request_uri)
-        req['app_key'] = ENV['OXFORD_APP_KEY']
-        req['app_id'] = ENV['OXFORD_APP_ID']
-        res = https.request(req)
-        puts res
-        if res.code == '200'
-          isWord = true
-        end
+        isWord = make_word_lookup(params[:input_text])
       end
       render json: { found: found, isWord: isWord }
     end
-
-    # # GET /boards/1
-    # def show
-    #   render json: @board
-    # end
-
-    # # POST /boards
-    # def create
-    #   @board = Board.new(board_params)
-    #   @board.generate_board()
-    #   if @board.save
-    #     render json: @board, status: :created
-    #   else
-    #     render json: @board.errors, status: :unprocessable_entity
-    #   end
-    # end
-
-    # # PATCH/PUT /boards/1
-    # def update
-    #   if @board.update(board_params)
-    #     render json: @board
-    #   else
-    #     render json: @board.errors, status: :unprocessable_entity
-    #   end
-    # end
-
-    # # DELETE /boards/1
-    # def destroy
-    #   @board.destroy
-    # end
 
     private
       # Use callbacks to share common setup or constraints between actions.
@@ -66,5 +24,19 @@ require 'openssl'
       # Only allow a trusted parameter "white list" through.
       def board_params
         params.require(:board).permit(:pretty_name)
+      end
+
+      def make_word_lookup(word)
+        url = URI.parse("https://od-api.oxforddictionaries.com/api/v1/entries/en/#{word}/regions=us")
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+        req = Net::HTTP::Get.new(url.request_uri)
+        req['app_key'] = ENV['OXFORD_APP_KEY']
+        req['app_id'] = ENV['OXFORD_APP_ID']
+        res = https.request(req)
+        if res.code == '200'
+          return true
+        end
+        return false
       end
   end
