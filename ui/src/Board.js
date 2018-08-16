74f6c7c9b9d1e4db4fc612b3ffe6d47a43aee6bc
Micah Zakahi <mzakahi@lessonomix.com>
2018-08-16 00:49:49 -0700

    add alert indicator on word found, add more routing

--- ui/src/Board.js
+++ ui/src/Board.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
 import _ from 'lodash';
 import axios from 'axios';
 
@@ -6,7 +8,8 @@ export default class Board extends Component {
   
   state = {
     board: {},
-    wordInput: ''
+    wordInput: '',
+    wordFound: null
   };
 
   componentDidMount() {
@@ -17,20 +20,26 @@ export default class Board extends Component {
   }
 
   findWord = e => {
+    e.preventDefault();
     axios.get(`http://localhost:3000/api/boards/${this.props.match.params.boardId}/find_input?input_text=${this.state.wordInput}`)
-      .then(result => console.log(result.data));
+      .then(result => this.setState({ wordFound: result.data.found }));
   }
 
   handleInput = e => {
-    console.log(e);
-    this.setState({ wordInput: e.target.value });
+    this.setState({ wordInput: e.target.value.toUpperCase(), wordFound: null });
   }
 
   render() {
     if (_.isEmpty(this.state.board)) return <div>LOADING...</div>
     return (
       <div className="container">
-        <h3>{this.state.board.pretty_name}</h3>
+        <div className="relative" style={{ width:'500px', margin: '0 auto' }}>
+          <Link to="/" style={{ position: 'absolute', left: '0', top: '5px' }}>
+            <i className="fas fa-long-arrow-alt-left" />
+            <span style={{ marginLeft:'5px' }}>Back</span>
+          </Link>
+          <h3>{this.state.board.pretty_name}</h3>
+        </div>
         <div className="row">
           <div className="boggle-board clearfix">
             {this.state.board.arrangement.map(x => {
@@ -48,12 +57,27 @@ export default class Board extends Component {
             })}
           </div>
         </div>
-        <div className="row">
-          <div className="col-xs-12">
-            <input type="text" name="findWord" onChange={this.handleInput} value={this.state.wordInput} />
-            <button name="submit" onClick={this.findWord}>Find Word</button>
+        <div className="row" style={{ marginTop: '15px' }}>
+          <div className="col-xs-12 form-group">
+            <form className="form-inline" onSubmit={this.findWord}>
+              <input type="text" className="form-control mb-2 mr-sm-2" onChange={this.handleInput} value={this.state.wordInput} style={{ width: '350px', marginRight: '5px' }} id="inlineFormInputName2" placeholder="Enter Word" />
+              <button type="submit" className="btn btn-primary">Find Word</button>
+            </form>
           </div>
         </div>
+        <div style={{ width: '500px', margin: '0 auto' }}>
+          {this.state.wordFound === true &&
+            <Alert bsStyle="success">
+              The word <strong>{this.state.wordInput}</strong> was found on the board.
+          </Alert>
+          }
+          {this.state.wordFound === false &&
+            <Alert bsStyle="danger">
+              <strong>Word Not Found.</strong> The word <strong>{this.state.wordInput}</strong> was not found on the board.
+          </Alert>
+          }
+        </div>
+        
       </div>
     )
   }
--- ui/src/List.js
+++ ui/src/List.js
@@ -5,7 +5,8 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 export default class List extends Component {
 
   state = {
-    boards: []
+    boards: [],
+    newBoardName: ''
   }
 
   componentDidMount() {
@@ -15,22 +16,50 @@ export default class List extends Component {
       }));
   }
 
+  handleCreateSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/api/boards', {
+        board: {
+          pretty_name: this.state.newBoardName
+        }
+      })
+      .then(result => this.props.history.push(`/board/${result.data.id}`));
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   handleListClick = boardId => {
-    console.log(boardId);
+    this.props.history.push(`/board/${boardId}`);
   }
 
   render() {
     return (
       <div className="container">
-        <h2>Saved Boggle Boards</h2>
-        <div className="row">
-          <div className="col-xs-12">
-            <ListGroup>
-            {this.state.boards.map(x => (
-              <ListGroupItem onClick={() => this.handleListClick(x.id)} key={x.id}>{x.pretty_name}</ListGroupItem>
-            ))}
-            </ListGroup>
+        <div style={{ width:'500px', margin: '0 auto' }}>
+          <div className="row" style={{ marginTop: '25px' }}>
+            <div className="col-xs-12" style={{ paddingBottom: '15px' }}>
+              <div className="col-xs-12 form-group">
+                <form className="form-inline" onSubmit={this.handleCreateSubmit}>
+                  <input type="text" className="form-control mb-2 mr-sm-2" onChange={this.handleInputChange} value={this.state.newBoardName} style={{ width: '300px', marginRight: '5px' }} name="newBoardName" placeholder="Custom Board Name" />
+                  <button type="submit" className="btn btn-primary">Create New Board</button>
+                </form>
+              </div>
+            </div>
           </div>
+          <hr />
+          <h3>Saved Boggle Boards</h3>
+          <div className="row">
+            <div className="col-xs-12">
+              <ListGroup>
+                {this.state.boards.map(x => (
+                  <ListGroupItem onClick={() => this.handleListClick(x.id)} key={x.id}>{x.pretty_name}</ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </div>
+          
         </div>
       </div>
     )
